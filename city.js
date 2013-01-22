73021a335a8f7ef42eda5a3fb1c757f12393a25e
shaper-pmp <shaper.pmp@gmail.com>
2013-01-22 22:58:05 +0000

    Changed building roofs to Phong material as well
    Building walls make the whole animation too slow, but Phong road, ground
    & roofs show off the helicopter spotlight nicely and leaves the
    rendering still acceptably fast.

--- city.js
+++ city.js
@@ -127,6 +127,8 @@ Building.prototype.getMesh = function () {
     if(Math.abs(face.normal.x) == 1 || Math.abs(face.normal.z) == 1) {
       face_height = Math.abs(combined.vertices[face.a].y - combined.vertices[face.c].y);
       var texture = this.getWallTexture(face_width, face_height);
+      materials.push(new THREE.MeshLambertMaterial( { color: this.colour, map:texture } ));
+    
     }
     
     // Horizontal surfaces (roof)
@@ -134,11 +136,12 @@ Building.prototype.getMesh = function () {
       face_width = Math.abs(combined.vertices[face.a].x - combined.vertices[face.c].x);
       face_height = Math.abs(combined.vertices[face.a].z - combined.vertices[face.c].z);
       var texture = this.getRoofTexture(face_width, face_height);
+      materials.push(new THREE.MeshPhongMaterial( { color: this.colour, map:texture } ));
+    
     }
    
     //console.log("Pushing texture for face %i @ %ix%i:", i, face_width, face_height);
     //console.log(texture);
-    materials.push(new THREE.MeshLambertMaterial( { color: this.colour, map:texture } ));
     face.materialIndex = i; /* And make sure we set the face to use the appropriate materialIndex (merged geometries apparently maintain their original materiaIndex, so without this step all of the building parts' faces only point at the first six textures!) */
   }
   
